a57d18936da07f3ff8ca3728be83b5cd2b504cf7
MarcusChr <mail@marcuslc.com>
2020-11-23 11:21:47 +0100

    Changed connection from a local DB to a database hosted on hildur.ucn.dk.

--- RESTful server/RESTfulService/Models/IdentityModels.cs
+++ RESTful server/RESTfulService/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace RESTfulService.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DbConnection", throwIfV1Schema: false)
         {
         }
         
