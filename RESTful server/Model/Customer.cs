a690fcf3f4a99ee7e78b7d33acdfd1e63201e230
Camilla Grubak Zdrinka Clausen <1080584@ucn.dk>
2020-11-27 11:58:32 +0100

    update i Customer

--- RESTful server/Model/Customer.cs
+++ RESTful server/Model/Customer.cs
@@ -9,7 +9,7 @@ namespace Model
     public class Customer : Person
     {
         public int CustomerID { get; set; }
-        public List<Reservation> Reservations { get; set;  }
+        public List<Reservation> Reservations { get; set; }
         //New Customer
         public Customer(string firstName, string lastName, string phone, string address, int postalCode, string city) : base(firstName, lastName, phone, address, postalCode, city)
         {
