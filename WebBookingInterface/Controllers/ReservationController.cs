f63e72ada7256d876361fc6d57a65b942c2ea26c
patrickmikkelsen <62879738+priox98@users.noreply.github.com>
2020-12-03 14:14:11 +0100

    Finally working- some reservation stuff added

---
+++ WebBookingInterface/Controllers/ReservationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBookingInterface.Controllers
+{
+    public class ReservationController : Controller
+    {
+        // GET: Reservation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult FindPersonalReservations()
+        {
+            return View();
+        }
+    }
+}
