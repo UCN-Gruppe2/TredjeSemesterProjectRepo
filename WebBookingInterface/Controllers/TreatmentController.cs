9b5281712cc3e67f7d4c538a637eefd83d0dcedc
Marcus Lausten Christensen <1080616@ucn.dk>
2020-12-10 10:15:01 +0100

    Added Treatment_DTO, added TreatmentController and added views.

--- WebBookingInterface/Controllers/TreatmentController.cs
+++ WebBookingInterface/Controllers/TreatmentController.cs
@@ -13,5 +13,10 @@ namespace WebBookingInterface.Controllers
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
